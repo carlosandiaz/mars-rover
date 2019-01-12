f6110b54c367f65b26f1320ab630ac58e4d4ec67
carlosandiaz <carlosandiaz@gmail.com>
2019-01-12 13:31:03 +0100

    /

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,14 +1,57 @@
 // Rover Object Goes Here
 // ======================
 
-// ======================
+
+var rover = {
+  direction: "N",
+  }
+
+
+// Turn left
+
 function turnLeft(rover){
+  
   console.log("turnLeft was called!");
+  
+    switch (rover.direction) {
+      case "N":
+      rover.direction = "W";
+      break;
+      case "E":
+      rover.direction = "N";
+      break;
+      case "S":
+      rover.direction = "E";
+      break;
+      case "W":
+      rover.direction = "S";
+      break;
+  }
 }
 
+
+// Turn right
+
 function turnRight(rover){
+
   console.log("turnRight was called!");
+
+  switch (rover.direction) {
+    case "N":
+    rover.direction = "E";
+    break;
+    case "E":
+    rover.direction = "S";
+    break;
+    case "S":
+    rover.direction = "W";
+    break;
+    case "W":
+    rover.direction = "N";
+    break;
+  }
 }
+
 
 function moveForward(rover){
   console.log("moveForward was called")
